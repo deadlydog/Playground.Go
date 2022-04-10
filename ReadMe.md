29af4fa2dc94a21a1f0785fa25be383b0848be8b
deadlydog <deadlydog@hotmail.com>
2022-04-10 10:49:57 -0600

    Initialize repo with boilerplate files and some Go code

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+# Playground.Go
+
+This is just a a place for me to play around with Go.
+
+Following [the W3Schools Go tutorial](https://www.w3schools.com/go/index.php).
+
+## Installation / Setup
+
+1. Install Go from <https://golang.org/dl/>.
+1. Install Go VS Code extension.
+1. Run the `Go: Install/Update Tools` command in VS Code.
+1. In your project directory, open a terminal and run `go mod init example.com/hello`.
---
+++ src/HelloWorld.go
@@ -0,0 +1,39 @@
+// Every program is part of a package (kind of like a namespace in C#).
+package main
+import ("fmt")
+
+const PI = 3.14
+
+func main() {
+	fmt.Println("Hello World!")
+
+	fmt.Println(PI)
+
+	// Multiple ways to declare variables.
+	var student1 string = "John" //type is string
+	var student2 = "Jane" //type is inferred
+	x := 2 //type is inferred
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+
+	// Can declare multiple variables of the same type at once.
+	var a, b, c, d int = 1, 3, 5, 7
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+	// Can declare multiple variables of the different types at once.
+	var (
+		e int
+		f int = 1
+		g string = "hello"
+	)
+
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+}
